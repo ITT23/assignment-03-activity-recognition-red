ff4368e115dd5bde5fe6517c3db1cb3786d1f446
MatthiasDobiosz <matthido98@web.de>
2023-05-09 17:56:52 +0200

    added DIPPID

--- gather-data.py
+++ gather-data.py
@@ -1,1 +1,20 @@
 # this program gathers sensor data
+import time
+from DIPPID import SensorUDP
+
+# use UPD (via WiFi) for communication
+PORT = 5700
+sensor = SensorUDP(PORT)
+
+capture_data = False
+start_time = 0
+
+while (True):
+    if sensor.has_capability('button_1'):
+        button_1 = sensor.get_value('button_1')
+        if int(button_1) == 1:
+            capture_data = True
+            start_time = time.time()
+
+        if capture_data:
+            print(time.time() - start_time)
