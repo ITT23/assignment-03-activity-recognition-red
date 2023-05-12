df7f9a2cdaaae6cb699746e0aee5685a8d32ae8c
Sina Schreiner <sina.schreiner01@gmail.com>
2023-05-12 09:07:03 +0200

    new datasets and cleaner code

--- activity-recognizer.py
+++ activity-recognizer.py
@@ -8,7 +8,7 @@ from DIPPID import SensorUDP
 import time
 
 # use UPD (via WiFi) for communication
-PORT = 5700
+PORT = 5701
 sensor = SensorUDP(PORT)
 
 
@@ -34,28 +34,27 @@ scaled_samples = scale(sensor_data)
 
 data_mean = sensor_data.copy()
 
-data_mean[['accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z']] = scaled_samples
+data_mean = scaled_samples
 
-print(data_mean.head())
+print(data_mean[:5])
 
 # normalization: map all values to a certain range
 s = MinMaxScaler()
-s.fit(data_mean[['accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z']])
-scaled_samples = s.transform(data_mean[['accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z']])
+s.fit(data_mean)
+scaled_samples = s.transform(data_mean)
 
 data_normalized = data_mean.copy()
 
-data_normalized[['accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z']] = scaled_samples
+data_normalized = scaled_samples
 
-print(data_normalized.head())
+print(data_normalized[:5])
 
-X_train, X_test, y_train, y_test = train_test_split(data_normalized[['accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z']], y, test_size=0.2,random_state=109) # 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(data_normalized, y, test_size=0.2,random_state=109) # 80% training and 20% test
 
 classifier = svm.SVC(kernel='linear')
 #classifier = svm.SVC(kernel='rbf') # non-linear classifier
 
-
-classifier.fit(X_train.values, y_train)
+classifier.fit(X_train, y_train)
 
 #y_pred_train = classifier.predict(X_train)
 y_pred_test=classifier.predict(X_test)
@@ -63,17 +62,22 @@ y_pred_test=classifier.predict(X_test)
 print('Model accuracy score with linear kernel and C=1000.0 : {0:0.4f}'. format(accuracy_score(y_test, y_pred_test)))
 
 while(True):
-    acc_x = float(sensor.get_value('accelerometer')['x'])
-    acc_y = float(sensor.get_value('accelerometer')['y'])
-    acc_z = float(sensor.get_value('accelerometer')['z'])
 
-    gyr_x = float(sensor.get_value('gyroscope')['x'])
-    gyr_y = float(sensor.get_value('gyroscope')['y'])
-    gyr_z = float(sensor.get_value('gyroscope')['z'])
+    if sensor.has_capability('accelerometer'):
+        acc_x = float(sensor.get_value('accelerometer')['x'])
+        acc_y = float(sensor.get_value('accelerometer')['y'])
+        acc_z = float(sensor.get_value('accelerometer')['z'])
 
-    grav_x = float(sensor.get_value('gravity')['x'])
-    grav_y = float(sensor.get_value('gravity')['y'])
-    grav_z = float(sensor.get_value('gravity')['z'])
+    if sensor.has_capability('gyroscope'):
+        gyr_x = float(sensor.get_value('gyroscope')['x'])
+        gyr_y = float(sensor.get_value('gyroscope')['y'])
+        gyr_z = float(sensor.get_value('gyroscope')['z'])
+
+    if sensor.has_capability('gravity'):
+        grav_x = float(sensor.get_value('gravity')['x'])
+        grav_y = float(sensor.get_value('gravity')['y'])
+        grav_z = float(sensor.get_value('gravity')['z'])
+
 
     pred = classifier.predict([[acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, grav_x, grav_y, grav_z]])
     print(encoder.classes_[pred])
--- gather-data.py
+++ gather-data.py
@@ -2,16 +2,15 @@
 import time
 from DIPPID import SensorUDP
 import csv
-import helpers
 
 # use UPD (via WiFi) for communication
-PORT = 5700
+PORT = 5701
 sensor = SensorUDP(PORT)
 
 capture_data = False
 start_time = 0
-label = "punching"
-number = 4
+label = "waving"
+number = 3
 
 headers= ['timestamp', 'accelerometer_x', 'accelerometer_y', 'accelerometer_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', 'gravity_x', 'gravity_y', 'gravity_z', 'activity']
 data =[]
@@ -38,17 +37,20 @@ while (True):
         if capture_data:
             duration = round(time.time() - start_time, 2)
 
-            acc_x = float(sensor.get_value('accelerometer')['x'])
-            acc_y = float(sensor.get_value('accelerometer')['y'])
-            acc_z = float(sensor.get_value('accelerometer')['z'])
+            if sensor.has_capability('accelerometer'):
+                acc_x = float(sensor.get_value('accelerometer')['x'])
+                acc_y = float(sensor.get_value('accelerometer')['y'])
+                acc_z = float(sensor.get_value('accelerometer')['z'])
 
-            gyr_x = float(sensor.get_value('gyroscope')['x'])
-            gyr_y = float(sensor.get_value('gyroscope')['y'])
-            gyr_z = float(sensor.get_value('gyroscope')['z'])
+            if sensor.has_capability('gyroscope'):
+                gyr_x = float(sensor.get_value('gyroscope')['x'])
+                gyr_y = float(sensor.get_value('gyroscope')['y'])
+                gyr_z = float(sensor.get_value('gyroscope')['z'])
 
-            grav_x = float(sensor.get_value('gravity')['x'])
-            grav_y = float(sensor.get_value('gravity')['y'])
-            grav_z = float(sensor.get_value('gravity')['z'])
+            if sensor.has_capability('gravity'):
+                grav_x = float(sensor.get_value('gravity')['x'])
+                grav_y = float(sensor.get_value('gravity')['y'])
+                grav_z = float(sensor.get_value('gravity')['z'])
 
             data.append([duration, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, grav_x, grav_y, grav_z, label])
 
